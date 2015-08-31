7913b75d1cdca9236c9264a7a455773e2ec46ff7
atypicalrobot <github@atypicalrobot.com>
2015-08-31 19:50:12 +0100

    new code

--- app.py
+++ app.py
@@ -7,12 +7,32 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# error handling
+app.config["DEBUG"] = True
+
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
 def hello_world():
     # define the view using a function, which returns a string
-    return "Hello, World!"
+    return "Hello, World!??!?!"
+
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+
+# dynamic route with explicit status codes
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
+
 
 # start the development server using the run() method
 if __name__ == "__main__":
